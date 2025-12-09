cd83edf9232df8a8eb9b7178f4dc760e7d705168
James Kibrick <jamesgk78@gmail.com>
2025-12-08 20:22:24 -0500

    utility set up and example file

---
+++ src/utils/elevation_api.py
@@ -0,0 +1,71 @@
+import requests
+import numpy as np
+
+# url = "https://api.open-elevation.com/api/v1/lookup"
+
+# response = requests.post(url, json={"locations": coords})
+# response.raise_for_status()
+# data = response.json()
+
+def get_elevation(coords: list, url="https://api.open-elevation.com/api/v1/lookup"):
+# {
+#    "results":
+#    [
+#       {
+#          "longitude":10.0,
+#          "elevation":515,
+#          "latitude":10.0
+#       },
+#       {
+#          "longitude":20.0,
+#          "elevation":545,
+#          "latitude":20.0
+#       },
+#       {
+#          "latitude":41.161758,
+#          "elevation":117,
+#          "longitude":-8.583933
+#       }
+#    ]
+# }
+    response = requests.post(url, json={"locations": coords})
+    response.raise_for_status()
+    data = response.json()
+    return data
+
+def build_coords(lat, lon, n_points=20, span=0.02):
+    "Returns lat and longtitude and builds coordinates for get elevation request."
+    lats = np.linspace(lat - span, lat + span, n_points)
+    lons = np.linspace(lon - span, lon + span, n_points)
+    coords = [{"latitude": lat, "longitude": lon} for lat in lats for lon in lons]
+    return coords
+
+def parse_elevation_response(data):
+    # "input looks like # {
+#    "results":
+#    [
+#       {
+#          "longitude":10.0,
+#          "elevation":515,
+#          "latitude":10.0
+#       },
+#       {
+#          "longitude":20.0,
+#          "elevation":545,
+#          "latitude":20.0
+#       },
+#       {
+#          "latitude":41.161758,
+#          "elevation":117,
+#          "longitude":-8.583933
+#       }
+#    ]
+# }"
+    results = []
+    # results_dic = defaultdict(list)
+    for response in data["results"]:
+        longitude = response.get("longitude")
+        elevation = response.get("elevation")
+        latitudes = response.get("latitude")
+        results.append(longitude, elevation, latitudes)
+    pass
